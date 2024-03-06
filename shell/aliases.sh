2f68af48c6e8a1aec5680ac44a342f8cf2d58d1f
Ingo Karkat <swdev@ingo-karkat.de>
2024-03-06 17:56:54 +0100

    Add alias support for inputToArg, linesToArg, ...

---
+++ shell/aliases.sh
@@ -0,0 +1,40 @@
+eval "$(runWithPrompt --addAliasSupport augmentLines \
+    '12pr' \
+    'stdout|stderr|prepend|run-if-empty' \
+    '' \
+    'between-command|progress'
+)"
+addAliasSupport inputToArg \
+    'r' \
+    'run-if-empty'
+
+eval "$(runWithPrompt --addAliasSupport linesToArg \
+    '' \
+    '' \
+    '' \
+    'between-command|progress'
+)"
+addAliasSupport linesToArgs \
+    'r' \
+    'run-if-empty'
+
+addAliasSupport placeholderArgument
+addAliasSupport placeholderArguments \
+    'r' \
+    'no-run-if-empty' \
+    'n' \
+    'command-arguments'
+
+addAliasSupport succeedsOrRetryArg \
+    '' \
+    'first-failure|either-failure' \
+    'r' \
+    'retry-command|retry-exec'
+addAliasSupport withConcatenatedInput \
+    '' \
+    'truncate-on-failure|truncate-on-success' \
+    'iX' \
+    'basedir|base-type|id|truncate-on'
+addAliasSupport ytee \
+    'iors' \
+    'input-buffered|output-buffered|reverse|sequential'
